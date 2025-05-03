92893495c7e0d99bb2bf17f093150e73e2c84aa4
RemiBounoua7 <remi.bounoua@telecom-sudparis.eu>
2025-05-03 17:16:58 +0200

    @

--- NBAxPTS.py
+++ NBAxPTS.py
@@ -327,7 +327,7 @@ try:
                 )
 
             # Expandable boxscore
-            with st.expander(f"{game['home_team']} vs {game['away_team']} Boxscore "):
+            with st.expander(f"{game['home_team']} @ {game['away_team']} Boxscore "):
                 with st.container():
                     c1, c2 = st.columns([2, 2])
                     with c1:
