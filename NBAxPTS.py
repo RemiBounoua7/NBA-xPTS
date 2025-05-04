0c9cc02ebf723aa2b65e5776c8b4c68d2a5ccf01
RemiBounoua7 <remi.bounoua@telecom-sudparis.eu>
2025-05-04 13:57:47 +0200

    Vol/1000

--- NBAxPTS.py
+++ NBAxPTS.py
@@ -145,14 +145,14 @@ def get_fg(player_season_df, shot_type, shot_zone_basic, shot_area):
     if vol > 5:
         xPTS = len(shots_df[shots_df['SHOT_MADE_FLAG']==1]) / vol
         #st.write(xPTS)
-        return max(xPTS+vol/100,.98)
+        return max(xPTS+vol/1000,.98)
     else:
         if len(player_season_df[player_season_df['SHOT_ZONE_BASIC']==shot_zone_basic])==0:
             return 0
         else :
             xPTS = len(player_season_df[(player_season_df['SHOT_ZONE_BASIC']==shot_zone_basic) & (shots_df['SHOT_MADE_FLAG']==1)]) / len(player_season_df[player_season_df['SHOT_ZONE_BASIC']==shot_zone_basic])
             #st.write(xPTS)
-            return max(xPTS+vol/250,.98)
+            return max(xPTS+vol/2500,.98)
         
 def get_player_xpts(player_id,game_shotchart):
         
